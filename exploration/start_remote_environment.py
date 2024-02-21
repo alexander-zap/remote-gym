5acbca2b3b5a50bfa95008cc6b80033b03c3e085
a_zap <test@test>
2024-02-21 14:33:42 +0200

    Put changeable constants at the top of the script

--- exploration/start_remote_environment.py
+++ exploration/start_remote_environment.py
@@ -5,13 +5,21 @@ import gymnasium as gym
 from remote_gym import start_as_remote_environment
 from remote_gym.util import configure_logging
 
+URL = "localhost"
+PORT = 56789
+SERVER_CREDENTIALS_PATHS = ("..\\ssl\\server.pem", "..\\ssl\\server-key.pem", None)
+
 configure_logging()
 
 if __name__ == "__main__":
     example_gym_env = gym.make("Acrobot-v1", render_mode="rgb_array")
 
     server = start_as_remote_environment(
-        url="localhost", port=56789, local_environment=example_gym_env, enable_rendering=True
+        local_environment=example_gym_env,
+        url=URL,
+        port=PORT,
+        server_credentials_paths=SERVER_CREDENTIALS_PATHS,
+        enable_rendering=True,
     )
 
     try:
