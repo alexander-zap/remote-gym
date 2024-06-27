6ba7697df27973860aad42a810f708d684844302
a_zap <test@test>
2024-06-27 11:50:26 +0200

    refactored exploration scripts to use argparse

--- exploration/start_environment_interaction.py
+++ exploration/start_environment_interaction.py
@@ -1,40 +1,85 @@
+import argparse
+import logging
+import sys
+
 import numpy as np
 from tqdm import tqdm
 
 from remote_gym import RemoteEnvironment
-from remote_gym.util import configure_logging
 
-URL = "localhost"
-PORT = 56789
-CLIENT_CREDENTIALS_PATHS = ("..\\ssl\\ca.pem", None, None)
+N_INTERACTION_EPISODES = 100
 
-N_EVALUATION_EPISODES = 100
-
-configure_logging()
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+root.addHandler(handler)
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-u",
+        "--url",
+        type=str,
+        help="URL of the machine on which the environment should be hosted",
+        default="localhost",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        help="Port of the environment hosting machine for gRPC communication",
+        default=56789,
+    )
+    parser.add_argument(
+        "--client_certificate",
+        type=str,
+        help="Path to TSL client certificate (optional, only for client authentication)",  # client.pem
+        default=None,
+    )
+    parser.add_argument(
+        "--client_private_key",
+        type=str,
+        help="Path to TLS client private key (optional, only for client authentication)",  # client-key.pem
+        default=None,
+    )
+    parser.add_argument(
+        "--root_certificate",
+        type=str,
+        help="Path to the root certificate (for TLS authentication)",  # ca.pem
+        default=None,
+    )
+    parser.add_argument(
+        "-r",
+        "--render_mode",
+        action="store_true",
+        help="Render mode of the environment being interacted with",
+        default=None,
+    )
+
+    args = parser.parse_args()
+
+    url = args.url
+    port = args.port
+    client_credentials_paths = (args.root_certificate, args.client_certificate, args.client_private_key)
+
     # Create instance for managing communication with remotely running environment
     environment = RemoteEnvironment(
-        url=URL,
-        port=PORT,
-        client_credentials_paths=CLIENT_CREDENTIALS_PATHS,
-        render_mode="human",
+        url=url,
+        port=port,
+        client_credentials_paths=client_credentials_paths if any(client_credentials_paths) else None,
+        render_mode=None,
     )
 
     # Print some environment information (observation and action space)
-    print("_____OBSERVATION SPACE_____ \n")
-    print("Observation Space Shape", environment.observation_space.shape)
-    print("Sample observation", environment.observation_space.sample())
-
-    print("\n _____ACTION SPACE_____ \n")
-    print("Action Space Shape", environment.action_space.shape)
-    print("Action Space Sample", environment.action_space.sample())
-
-    print("\n _____REWARD RANGE_____ \n")
-    print("Reward Range Interval", environment.reward_range)
+    print("Observation Space: ", environment.observation_space)
+    print("Action Space: ", environment.action_space)
+    print("Reward Range: ", environment.reward_range)
 
     episode_rewards = []
-    for episode in tqdm(range(N_EVALUATION_EPISODES)):
+    for episode in tqdm(range(N_INTERACTION_EPISODES)):
         episode_reward = 0
         prev_observation, _ = environment.reset()
         environment.render()
@@ -62,5 +107,3 @@ if __name__ == "__main__":
     std_reward = np.std(episode_rewards)
 
     print(f"mean_reward={mean_reward:.2f} +/- {std_reward:.2f}")
-
-    environment.__del__()
--- exploration/start_remote_environment.py
+++ exploration/start_remote_environment.py
@@ -1,25 +1,77 @@
+import argparse
 import logging
+import sys
 
 import gymnasium as gym
 
 from remote_gym import start_as_remote_environment
-from remote_gym.util import configure_logging
 
-URL = "localhost"
-PORT = 56789
-SERVER_CREDENTIALS_PATHS = ("..\\ssl\\server.pem", "..\\ssl\\server-key.pem", None)
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+root.addHandler(handler)
 
-configure_logging()
 
 if __name__ == "__main__":
-    example_gym_env = gym.make("Acrobot-v1", render_mode="rgb_array")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-u",
+        "--url",
+        type=str,
+        help="URL of the machine on which the environment should be hosted",
+        default="localhost",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        help="Port of the environment hosting machine for gRPC communication",
+        default=56789,
+    )
+    parser.add_argument(
+        "-r",
+        "--enable_rendering",
+        action="store_true",
+        help="Flag for enabling rendering on the remote environment. "
+        "If set to True, make sure that the passed environment has its .render_mode attribute set to 'rgb_array'.",
+        default=False,
+    )
+    parser.add_argument(
+        "--server_certificate",
+        type=str,
+        help="Path to the self-signed server certificate (for TLS authentication)",  # server.pem
+        default=None,
+    )
+    parser.add_argument(
+        "--server_private_key",
+        type=str,
+        help="Path to the self-signed server private key (for TLS authentication)",  # server-key.pem
+        default=None,
+    )
+    parser.add_argument(
+        "--root_certificate",
+        type=str,
+        help="Path to the root certificate (for TLS authentication)",  # ca.pem
+        default=None,
+    )
+    args = parser.parse_args()
+
+    url = args.url
+    port = args.port
+    server_credentials_paths = (args.server_certificate, args.server_private_key, args.root_certificate)
+    enable_rendering = args.enable_rendering
+
+    example_environment = gym.make("Acrobot-v1", render_mode="rgb_array")
 
     server = start_as_remote_environment(
-        local_environment=example_gym_env,
-        url=URL,
-        port=PORT,
-        server_credentials_paths=SERVER_CREDENTIALS_PATHS,
-        enable_rendering=True,
+        local_environment=example_environment,
+        url=url,
+        port=port,
+        server_credentials_paths=server_credentials_paths if any(server_credentials_paths) else None,
+        enable_rendering=enable_rendering,
     )
 
     try:
